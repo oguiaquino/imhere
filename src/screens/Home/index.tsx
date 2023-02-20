a285b622659f3e2f1bc0fb22a31fd29103279fca
Guilherme Aquino <guiiaquino92@gmail.com>
2023-02-20 19:19:09 -0300

    feat: aprendendo sobre Flatlist

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,45 +1,61 @@
-import {
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+
 import { Participant } from "../components/Participant";
 
 import { styles } from "./style";
 
-export default function Home() {
+export function Home() {
+  const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João'];
+  
   function handleParticipantAdd() {
-    console.log("hi");
+    console.log("Você clicou no botão de Adicionar!");
   }
 
-  function hanleParticipantRemove() {}
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
 
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
 
       <View style={styles.form}>
-        <TextInput
+        <TextInput 
           style={styles.input}
           placeholder="Nome do participante"
-          placeholderTextColor="#6b6b6b"
+          placeholderTextColor="#6B6B6B"
         />
-
+        
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-          <Text style={styles.buttonText}>+</Text>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Participant
-          name="Guilherme Aquino"
-          onRemove={hanleParticipantRemove}
-        />
-      </ScrollView>
+      <FlatList 
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant 
+            key={item} 
+            name={item} 
+            onRemove={() => handleParticipantRemove("Rodrigo")} 
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
     </View>
-  );
+  )
 }
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     padding: 24
   },
   eventName: {
-    color: '#FDFCFE',
+    color: '#FFF',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48
@@ -17,32 +17,36 @@ export const styles = StyleSheet.create({
     fontSize: 16
   },
   input: {
-    flex:1,
+    flex: 1,
     height: 56,
     backgroundColor: '#1F1E25',
     borderRadius: 5,
-    color: '#FDFCFE',
+    color: '#FFFF',
     padding: 16,
     fontSize: 16,
-    marginRight: 12,
+    marginRight: 12
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   form: {
     width: '100%',
     flexDirection: 'row',
     marginTop: 36,
-    marginBottom: 42,
+    marginBottom: 42
   },
-  button: {
-  width: 56,
-    height: 56,
-    borderRadius: 5,
-    backgroundColor: '#31CF67',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
+  listEmptyText: {
     color: '#FFF',
-    fontSize:24,
+    fontSize: 14,
+    textAlign: 'center'
   }
-
 })
